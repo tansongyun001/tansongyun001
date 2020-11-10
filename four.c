ea3110c1971fc65c85a50474cd9c97f6276d65a3
tansongyun001 <2533155535@qq.com>
2020-11-10 08:55:05 +0800

    four.c

---
+++ four.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int i,n;
+	printf("This program prints a table of squares.\n");
+	printf("Enter number of entries in table:");
+	scanf ("%d",&n);
+	i=1;
+	while (i<=5){
+	  printf ("%10d%10d\n",i,i*i);
+      i++;}
+    return 0;
+ } 

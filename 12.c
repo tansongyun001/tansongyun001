4e544c6648e5f9673b1bb1b00be3b29ad41fceef
tansongyun001 <2533155535@qq.com>
2020-11-16 23:17:18 +0800

    12.c

---
+++ 12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (void)
+{
+	int n,i=1;
+	printf("这是一个验证数字是否大于1的算法：\n");
+	printf ("请输入整数n：");
+	scanf("%d",&n);
+    if (n>i)
+    printf ("n比1大");
+	else
+	printf ("n不比1大") ; 
+	return 0;
+}

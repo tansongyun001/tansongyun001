d819f2970d996a79e6d734c136b4069a131a24f5
tansongyun001 <2533155535@qq.com>
2020-12-31 23:00:57 +0800

    a

---
+++ A.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int old(int a,int k)
+{
+	int n;
+	old(int a,int k)=(n-1)*k;
+	return Old(int a,int k)+a;
+	
+}
+int main (void)
+{
+	int n,a,k;
+	if(n<=0||k>100||n<2)
+	  printf("Wrong Number\n");
+	else
+	  printf("%d",old(int a,int k));
+	return 0;
+}

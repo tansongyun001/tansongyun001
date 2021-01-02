66014080eb3953207289bf04d5832e9317917a9a
tansongyun001 <2533155535@qq.com>
2021-01-02 23:29:44 +0800

    lianxi2c

---
+++ kaoshi2c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (void)
+{
+	int n,i,a,b,c=0;
+	scanf("%d",n);
+	for(i=0;i<n;i++)
+	  {
+	  	scanf("%d",&a);
+	  	do{b=a/10;
+	  	   c++;
+		  }while (b>0)
+	  	
+	  }
+}
